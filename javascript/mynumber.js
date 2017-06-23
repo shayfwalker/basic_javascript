4038607b7e721ae671c8c55e8746aa8868274615
Shayfanise Walker <shayfanisewalker@Shayfanises-MacBook-Air.local>
2017-06-22 20:54:41 -0700

    Initial

---
+++ javascript/mynumber.js
@@ -0,0 +1,115 @@
+//nameSwap
+
+// var myNumber = 42;
+// var myName = 'Shay';
+
+// myName = myNumber;
+
+// myNumber = myName;
+
+// console.log(myNumber);
+// // console.log(myName);
+
+//Numbers -52 to 1066
+// for(var i = -52; i < 1066; i++ ){
+// 	console.log(i)
+// }
+
+// goodmorning 98 times 
+// function beCheerful(){
+// for (var i = 1; i < 99; i++) {
+	
+// console.log('good morning!');
+// 	}
+// }
+
+// beCheerful();
+
+
+// for(var i = -300 ; i < 0 ; i++){
+// 	if ( i % 3 == 0){
+// 		console.log(i);
+// 	}
+// }
+
+//While
+// var num = 2000;
+// while(num < 5280){
+// 	num = num + 1;
+// 	console.log(num);
+// }
+
+
+// function yourBirthday(a,b){
+// 	a = 1;
+// 	b = 3;
+// 	if(a == 1 && b == 3 ){
+// 		console.log("How did you kno??");
+// 	}
+// 	else{
+// 		console.log('Just another day');
+// 	}
+	
+// }
+
+// function leapYear(){
+// 	for(var i = 1; i< 401; i++){
+// 		if( i % 4 == 0){
+// 			console.log("It's a leap year");
+// 		}
+// 		else if(i % 100 == 0){
+// 			console.log("It's not a leap year");
+// 		}
+// 	}
+// }
+// leapYear();
+
+
+// for(var i = 512 ; i < 4096 ; i++ ){
+// 	if( i % 5 == 0){
+// 		console.log(i);
+// 	}
+// }
+
+
+// While divisible by 6
+// var num = 6;
+// while(num < 60000){
+// 	num = num + 1;
+// 	if(num % 6 === 0){
+
+	
+// 	console.log(num); 
+// 	}
+// }
+
+//Coding Dojo
+// for( var i = 1 ; i < 100 ; i++){
+// 	if(i % 5 == 0){
+// 		console.log("Coding");
+// 	}
+// 	if (i % 10 == 0){
+// 		console.log("Dojo")
+// 	}
+// }
+
+
+// for(var i = -300000 ; i < 300000; i++){
+// 	if(i % 3 == 0){
+// 		i = i + 1;
+// 	}
+// 	console.log();
+// }
+
+
+// Countdown by Fours
+// var num = 2016;
+// while(num > 4){
+// 	num -= 4;
+// 	console.log(num);
+// }
+
+
+function finalCountdown(a,b,c,d){
+	return 
+}
